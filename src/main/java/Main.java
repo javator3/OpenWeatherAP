5942c3deba117d9b55dd1cd6ac48024d21760cd5
dragonskaju <dragonskaju@gmail.com>
2019-02-10 11:49:23 +0100

    Open Weather

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,31 @@
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import pl.sda.openwather.model.Weather;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        try {
+            URL jsonURI = new URL("http://api.apixu.com/v1/current.json?key=a497df960ef548f3b4590439191002&q=Paris");
+            ObjectMapper objectMapper = new ObjectMapper();
+            Weather weather = objectMapper.readValue(jsonURI, Weather.class);
+            System.out.println(weather);
+        }catch (MalformedURLException use){
+            use.printStackTrace();
+        } catch (JsonParseException e) {
+            e.printStackTrace();
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
 
