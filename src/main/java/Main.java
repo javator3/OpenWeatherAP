1b1bfef20343b9752d8a98ad2b3855bb6d243dc9
dragonskaju <dragonskaju@gmail.com>
2019-02-10 12:24:24 +0100

    Open Weather

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,21 +12,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        try {
-//            URL jsonURI = new URL("http://api.apixu.com/v1/current.json?key=a497df960ef548f3b4590439191002&q=Paris");
-//            ObjectMapper objectMapper = new ObjectMapper();
-//            Weather weather = objectMapper.readValue(jsonURI, Weather.class);
-//            System.out.println(weather);
-//        }catch (MalformedURLException use){
-//            use.printStackTrace();
-//        } catch (JsonParseException e) {
-//            e.printStackTrace();
-//        } catch (JsonMappingException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-
         WeatherService weatherService = new WeatherService("http://api.apixu.com/v1/current.json", "?key=a497df960ef548f3b4590439191002&q=");
         String weather = weatherService.getCityWeather("Rypin");
         System.out.println(weather);
